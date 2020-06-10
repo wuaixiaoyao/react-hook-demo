af89a6c8dce72488c56da50c38ca4868e3a7929b
yll <yangll-h@glodon.com>
2020-06-10 15:46:44 +0800

    feat: 代码优化

--- src/pages/home/banner.tsx
+++ src/pages/home/banner.tsx
@@ -2,45 +2,46 @@
  * @author wuaixiaoyao
  * @date 2019/8/30
  * @Description:
-*/
+ */
 import * as React from 'react';
-import {Toast} from 'antd-mobile'
-import styled from 'styled-components'
-import { Item } from '../../components/banner/components'
-import Carousel from '../../components/banner/index'
-import {getBannerList} from '../../api'
-import styles from './index.module.scss'
-const { useState, useCallback ,useEffect} = React;
+import { Toast } from 'antd-mobile';
+import styled from 'styled-components';
+import { Item } from '../../components/banner/components';
+import Carousel from '../../components/banner/index';
+import { getBannerList } from '../../api';
+import styles from './index.module.scss';
+const { useState, useCallback, useEffect } = React;
 
 type bannerItem = {
-    pic:string,
-    titleColor:'red'|'blue'
-}
+  pic: string;
+  titleColor: 'red' | 'blue';
+};
 
 const BannerWrapper = styled.div`
-   background: #cecece; 
-`
-export default function Banner () {
+  background: #cecece;
+`;
+export default function Banner() {
+  const [bannerList, setBannerList] = useState([]);
 
-    const [bannerList, setBannerList] = useState([])
+  useEffect(() => {
+    getList();
+  }, []);
 
-    useEffect( () => {
-        getList()
-    },[]);
+  const getList = async () => {
+    Toast.loading('加载中...');
+    let res = await getBannerList({ type: 2 });
+    let { banners: bannerList } = res;
+    setBannerList(bannerList);
+    Toast.hide();
+  };
 
-    const getList = async () => {
-        Toast.loading('加载中...')
-        let res = await getBannerList({type:2});
-        let {banners:bannerList} = res;
-        setBannerList(bannerList);
-        Toast.hide()
-    }
-
-    return <BannerWrapper>
-        <Carousel title="Carousel">
-            { bannerList.map((item:bannerItem,index:number) =>{
-                return  <Item key={index} img={item.pic} />
-            })}
-        </Carousel>       
+  return (
+    <BannerWrapper>
+      <Carousel title="Carousel">
+        {bannerList.map((item: bannerItem, index: number) => {
+          return <Item key={index} img={item.pic} />;
+        })}
+      </Carousel>
     </BannerWrapper>
+  );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,20 +2,20 @@
  * @author wuaixiaoyao
  * @date 2019/7/1
  * @Description:
-*/
+ */
 import { Action, createStore } from 'redux';
 import reducer from '../reducer/reducer';
 
 export interface IState {
-	lastUpdated: number;
-	todos: string[];
+  lastUpdated: number;
+  todos: string[];
 }
 
 export type Action = { type: 'add todo'; todo: string } | { type: 'delete todo'; index: number };
 
 export function makeStore() {
-	return createStore(reducer, {
-		lastUpdated: 0,
-		todos: [ 'Make the fire!', 'Fix the breakfast!', 'Wash the dishes!', 'Do the mopping!' ]
-	});
+  return createStore(reducer, {
+    lastUpdated: 0,
+    todos: ['Make the fire!', 'Fix the breakfast!', 'Wash the dishes!', 'Do the mopping!'],
+  });
 }
